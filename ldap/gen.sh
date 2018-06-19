ea4644fbc6497cf4a57c1cec27d38d8b34270c87
russ stevenson <russ.stevenson@gmail.com>
2018-06-19 09:00:58 -0700

    updates

--- ldap/gen.sh
+++ ldap/gen.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-./gen_people.sh
+./gen_users.sh
 ./gen_groups.sh
+
+
+
+#ldapadd -f users_add.ldif -D cn=Manager,dc=vlab,dc=local -w password
+#ldapadd -f groups_add.ldif -D cn=Manager,dc=vlab,dc=local -w password
