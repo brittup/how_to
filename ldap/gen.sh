7a70060d79dbf29c68513bf0f56ccffad934d2a9
russ stevenson <russ.stevenson@gmail.com>
2022-02-04 12:14:50 -0800

    updates

--- ldap/gen.sh
+++ ldap/gen.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+
+### run this file to create the base ldif's for users and groups
+
 ./gen_users.sh
 ./gen_groups.sh
 
 
+
+### use these commands to add users and groups into LDAP Server from the ldif's
 
 #ldapadd -f users_add.ldif -D cn=ldapadm,dc=demo,dc=local -w password
 #ldapadd -f groups_add.ldif -D cn=ldapadm,dc=demo,dc=local -w password
